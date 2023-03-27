3d0b9d001080982f21355c5a5337345cfe3174b0
Emilie <EmilieMontpre@outlook.com>
2023-03-28 01:58:21 +0200

    creation of the page "Timeline"

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/timeline', function () {
+    return view('timeline');
+})->middleware(['auth', 'verified'])->name('timeline');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
