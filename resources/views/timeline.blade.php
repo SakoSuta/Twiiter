9f56a2de1595ffb7044aba287befd4a1dd6b23a9
Emilie <EmilieMontpre@outlook.com>
2023-04-01 20:47:55 +0200

    front for tweet (not working)

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("No you're not !") }}
+                    <form action="" class="flex flex-col">
+                        <label for="" class="font-semibold">Share to the world what you want to say ! 🕊️</label>
+                        <textarea name="" id="" cols="30" rows="5" class="my-9 dark:bg-gray-900 rounded-lg focus:ring-indigo-500 border-gray-700 resize-none" maxlength="180" placeholder="Don't be shy !"></textarea>
+                    </form>
                 </div>
             </div>
         </div>
